e2393417a8a27e36a9a9a455bccef58de38e171a
Marina <marina-insightful@mail.ru>
2020-09-07 23:02:52 +0300

    pom.xml and Conditioner.java

--- src/main/java/Conditioner.java
+++ src/main/java/Conditioner.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
 }
+
+
+
+
